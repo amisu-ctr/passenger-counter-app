763d847b7c81216d5860b284e7414422af13348b
amisu-ctr <hamisurahman@gmail.com>
2023-03-06 23:13:47 +0000

    Add save function that stores previous counts onto a para tag keeping track of previous counts And sets count back to zero

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 let countEl = document.getElementById('count-el')
+let saveEl = document.getElementById('save-el')
 let count = 0
 
 function increment() {
     count++;
-    countEl.innerHTML = count
+    countEl.innerHTML = count;
+
 }
+
+function save() {
+    let countstr = count + ' - '
+    saveEl.innerHTML += countstr;
+    countEl.textContent = 0;
+    count = 0
+}
+
 
 
